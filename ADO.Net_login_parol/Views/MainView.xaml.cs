df19f849ba8dc8b4a4d6e1ac6980a87cdd1c8ad0
Mehdi_ha01 <Mehdi_ha01@STHQ0113-13>
2023-05-27 14:55:14 +0400

    Add project files.

---
+++ ADO.Net_login_parol/Views/MainView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ADO.Net_login_parol.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+        private void OpenSignUpWindow()
+        {
+            SignUp signUpWindow = new SignUp();
+            MainView mainView = new MainView();
+            Window window = new Window
+            {
+                Title = "Sign Up",
+                Content = signUpWindow,
+                Width = 800,
+                Height = 450
+            };
+            window.ShowDialog();
+
+            Window mainWindow = Window.GetWindow(mainView);
+            mainWindow.Close();
+        }
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenSignUpWindow();
+        }
+
+    }
+}
